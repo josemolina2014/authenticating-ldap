89fb138cd758a575d6e1f1f70dcbfa93d65aa7af
jlmolina <j.l.molina@cgiar.org>
2020-02-06 08:19:33 -0500

    uso de application.properties del proyecto

--- src/main/java/com/example/authenticatingldap/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/authenticatingldap/configuration/WebSecurityConfig.java
@@ -1,13 +1,24 @@
 package com.example.authenticatingldap.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
 @Configuration //Tags the class as a source of bean definitions for the application context.
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${ldap.server.url}")
+    private String AD_URL;
+
+    @Value("${ldap.servers.urls}")
+    private String[] LDAP_URLS;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,5 +41,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordCompare()
                 .passwordEncoder(new BCryptPasswordEncoder())
                 .passwordAttribute("userPassword");
+
+
+        System.out.println("AD_URL "+ AD_URL);
+        System.out.println("LDAP_URLS: " + LDAP_URLS[0]  +" - "+ LDAP_URLS[1] + "\tlength "+LDAP_URLS.length );
+
     }
+
+   /* @Bean
+    public AuthenticationProvider activeDirectoryLdapAuthenticationProvider() {
+        ActiveDirectoryLdapAuthenticationProvider provider = new ActiveDirectoryLdapAuthenticationProvider("bmad.com",
+                "ldap://192.168.1.166:389/DC=bmad,DC=com");
+        provider.setSearchFilter("OU=devs,OU=employees");
+        provider.setConvertSubErrorCodesToExceptions(true);
+        provider.setUseAuthenticationRequestCredentials(true);
+
+        return provider;
+    }*/
+
 }
---
+++ src/main/java/com/example/authenticatingldap/constant/Constants.java
@@ -0,0 +1,11 @@
+package com.example.authenticatingldap.constant;
+
+public class Constants {
+
+    public static final String HOST = "ciatroot3.CGIARAD.ORG";
+    public static final String[] DCLIST = {"ciatroot3.CGIARAD.ORG","ciatroot4.CGIARAD.ORG"};
+    public static final String PORT = "3268";
+
+    public static final String CIAT_DEFAULT_DOMAIN="OU=CIATHUB,DC=CGIARAD,DC=ORG";
+
+}
